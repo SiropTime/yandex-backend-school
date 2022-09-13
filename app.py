1c5b0241e0d41bfd68bcff477e0591701526df36
SiropTime <sirop4beats@gmail.com>
2022-09-13 18:43:52 +0300

    Some little fixes from laptop

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from utility import *
 
 app = Flask(__name__)
 app.config.from_pyfile("config.cfg")
+parent_node = Folder("0", "", "2010-02-03T00:00:00Z")
 
 
 @app.route('/')
@@ -20,17 +21,20 @@ def imports():
         node = ""
         if item["type"] == FOLDER:
             node = Folder(item["id"], item["parentId"], data["updateDate"])
+            parent_node.children[item["id"]] = node
+        elif item["type"] == FILE:
+            pass
 
     return "Success"
 
 
-@app.route('/delete/{id}', methods=["DELETE"])
-def delete():
+@app.route('/delete/<id>', methods=["DELETE"])
+def delete(id: str):
     pass
 
 
-@app.route('/nodes/{id}', methods=["GET"])
-def nodes():
+@app.route('/nodes/<id>', methods=["GET"])
+def nodes(id: str):
     pass
 
 
@@ -39,7 +43,7 @@ def updates():
     pass
 
 
-@app.route('/node/{id}/history', methods=["GET"])
-def node_history():
+@app.route('/node/<id>/history', methods=["GET"])
+def node_history(id: str):
     pass
 
