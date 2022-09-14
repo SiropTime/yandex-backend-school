293fdf4755c3e456d982f5b52148c582ac94a35c
SiropTime <sirop4beats@gmail.com>
2022-09-15 00:19:10 +0300

    First interactions with DB

--- __main__.py
+++ __main__.py
@@ -1,5 +1,7 @@
-from app import app
+from app import app, db, on_init
 
 
 if __name__ == '__main__':
+    db.create_all()
+    on_init()
     app.run(host="0.0.0.0", port=8080)
--- app.py
+++ app.py
@@ -1,11 +1,38 @@
 from flask import Flask, request, abort, Response
+from flask_sqlalchemy import SQLAlchemy
 
 from utility import *
-
 
 app = Flask(__name__)
 app.config.from_pyfile("config.cfg")
 parent_node = Folder("0", "", "2003-07-01T00:00:00Z")
+db = SQLAlchemy(app)
+
+
+# DTO
+class FileDB(db.Model):
+    id = db.Column(db.Text, primary_key=True)
+    type = db.Column(db.Text, nullable=False)
+    url = db.Column(db.Text, unique=True, nullable=False)
+    parent_id = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Integer, nullable=False)
+    update_date = db.Column(db.Text, nullable=False)
+
+    def __init__(self, id, type, url, parent_id, size, update_date):
+        self.id = id
+        self.type = type
+        self.url = url
+        self.parent_id = parent_id
+        self.size = size
+        self.update_date = update_date
+
+    def __repr__(self):
+        return f"<{FILE if self.type == FILE else FOLDER}, %r, %r>" % (self.id, self.url)
+
+
+def on_init():
+    results = FileDB.query.all()
+    logger.info(results)
 
 
 @app.route('/')
@@ -42,8 +69,10 @@ def imports():
             except KeyError as ex:
                 return abort(Response(f"Validation failed. '{ex.args[0]}' is required", 400))
 
-            node.children = dict([(n.id, n) for n in filter(lambda d: d["parentId"] == node.id, current_items)])
+            node.children = dict([(n["id"], n) for n in filter(lambda d: d["parentId"] == node.id, current_items)])
+            logger.info(node.children)
             parent_node.children[item["id"]] = node
+            db.session.add(FileDB(node.id, FOLDER, node.url, node.parent_id, node.size, node.update_date))
 
         elif item["type"] == FILE:
             try:
@@ -52,11 +81,12 @@ def imports():
                 return abort(Response(f"Validation failed. '{ex.args[0]}' is required", 400))
 
             parent_node.children[item["id"]] = node
+            db.session.add(FileDB(node.id, FILE, node.url, node.parent_id, node.size, node.update_date))
 
         else:
             return abort(Response("Validation failed. There is no needed type for imported files"), 400)
 
-
+    db.session.commit()
     return Response("Successfully added items", 200)
 
 
